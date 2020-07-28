b530b6375981d9913207a527f7b63ae106fe949b
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-28 21:47:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,26 @@
-class Dog 
+class Dog
   @@all = []
-  attr_accessor :name
- def initialize(name)
-   @@name = name 
-   @@all << self
- end
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+
   def self.all
     @@all
-  end 
- def self.clear_all
+  end
+
+  def self.clear_all
     @@all = [] 
   end
+
   def self.print_all
     @@all.each do |dog|
-      puts dog.name 
+      puts dog.name
     end
 end
-  
-  
-  
-  
+
+  def save
+    @@all << self
+  end
 end
